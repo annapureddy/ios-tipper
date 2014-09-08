c28214a4e39f8312473517864d5f26e58a0e3a37
annapureddy <siddhartha.annapureddy@gmail.com>
2014-09-08 16:53:42 -0700

    adding README.md and walkthrough.gif

---
+++ tipper/SettingsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.swift
+//  tipper
+//
+//  Created by Sid Reddy on 9/7/14.
+//  Copyright (c) 2014 Sid Reddy. All rights reserved.
+//
+
+import Cocoa
+
+class SettingsViewController: UIViewController {
+
+}
